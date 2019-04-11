2dbd73b98288867294ec989cfb3b0a1f8a4489c1
rezera <071bct538@pcampus.edu.np>
2019-04-11 09:22:03 +0545

    Update

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 code()
 {
-    until python loader.py --start $1 --end $2 ;do
+    until python3 loader.py --start $1 --end $2 ;do
         sleep 1;
     done
 }
@@ -9,6 +9,5 @@ for start in {0..19}
 do
     let st=$((1000000 + $(($start * 100000)) ))
     let end=$(( 1000000 + $(( $(($start + 1)) * 100000)) ))
-#    code "$st" "$end" &
-    echo $st, $end
+    code "$st" "$end" &
 done
